e2d2b690af883be8a681b9c0eda598f20efa13e7
AS <AS@ass-macbook-pro.local>
2016-04-14 11:02:33 +0800

    chusha

--- YXNeleaseNews/YXNeleaseNews/ViewController.m
+++ YXNeleaseNews/YXNeleaseNews/ViewController.m
@@ -18,7 +18,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
